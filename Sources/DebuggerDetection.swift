a510a9b20f109357f9a0d22b71591078a27c1d92
ha-ja <36563845+ha-ja@users.noreply.github.com>
2025-01-29 07:01:21 +0100

    feat: debugger detection added
    * add DebuggerDetection.swift
    
    * add DebuggerDetection to ThreatDetectionCenter
    
    * add debugger detection view to SecurityToolkitExample
    
    * add documentation for isDebuggerAttached
    
    * fix typo: isDebuggerAttached -> isDebuggerDetected
    
    * Comment made even more clear
    
    * hasTracerFlagSet() additionally made private
    
    * No explicit Darwin import needed and therefore rmd
    
    no explicit Darwin import needed and therefore removed
    
    * Use of optional bool for isDebuggerDetected return
    
    Use of optional bool for public function isDebuggerDetected.
    
    This decision is the result of a joint discussion that took place in the associated PR #10 :
    https://github.com/EXXETA/iOS-Security-Toolkit/pull/10
    
    Motivation:
    No exception should be thrown.
    It should be communicated that something went wrong in the decision-making process and that no clear statement can be made.
    
    Decision:
    We now return nil in an unclear case.
    nil: The detection process did not produce a definitive result. This could happen due to system limitations, lack of required permissions, or other undefined conditions.

---
+++ Sources/DebuggerDetection.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+// MARK: - Internal
+internal final class DebuggerDetection {
+    
+    static func threatDetected() -> Bool? {
+        hasTracerFlagSet()
+    }
+}
+
+// MARK: - Private
+fileprivate extension DebuggerDetection {
+    
+    /// Check P_TRACED flag from Darwin Kernel
+    /// if the process is traced
+    private static func hasTracerFlagSet() -> Bool? {
+        var info = kinfo_proc()
+        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()] // Kernel info, process info, specific process by PID, get current process ID
+        var size = MemoryLayout.stride(ofValue: info)
+        
+        let unixStatusCode = sysctl(&mib, u_int(mib.count), &info, &size, nil, 0)
+        
+        return unixStatusCode == 0 ? (info.kp_proc.p_flag & P_TRACED) != 0 : nil
+    }
+}
